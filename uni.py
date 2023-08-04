b83ad518a4950fa6af3a3b9cab59f4dcbdecbb6d
Mh <mhrj007846@gmail.com>
2023-08-04 23:18:50 +0330

    cleaned bfs

--- uni.py
+++ uni.py
@@ -365,6 +365,140 @@ import math
 
 
 
+       
+
+
+
+
+
+
+# bfs ======> 
+
+# q = queue()
+# color = ["w" for i in range(num_of_vertices)]
+# distance = [0 for i in range(num_of_vertices)]
+# p = [-1 for i in range(num_of_vertices)]
+# distance_matrices = []
+
+
+
+# for n in range(num_of_vertices):
+#     q.add(n)
+#     while q.give_length() - 1 >= 0:
+#         u = q.pop()
+#         if u != -1:
+#             for i in range(len(dic[u])):
+#                 j = dic[u][i]
+#                 if color[j] == "w":
+#                     color[j] = "g"
+#                     distance[j] = distance[u] + 1
+#                     p[j] = u
+#                     q.add(j)
+#             color[u] = "B"
+#     print()
+#     print(f"{n} + : + {color},{distance},{p}")
+#     distance_matrices.append(distance)
+#     color = ["w" for i in range(num_of_vertices)]
+#     distance = [0 for i in range(num_of_vertices)]
+#     p = [-1 for i in range(num_of_vertices)] 
+
+
+
+# def get_greatest_distance(dist_matrices):
+#     greatest_num = 0
+#     for i in range(num_of_vertices):
+#         for j in range(num_of_vertices):
+#             if(greatest_num < dist_matrices[i][j]):
+#                 greatest_num = dist_matrices[i][j]
+#     return greatest_num
+
+# print()
+# print(get_greatest_distance(distance_matrices))
+
+
+#==>
+
+# q.add(0)
+
+# while q.give_length() - 1 >= 0:
+#     u = q.pop()
+#     if u != -1:
+#         for i in range(len(dic[u])):
+#             j = dic[u][i]
+#             if color[j] == "w":
+#                 color[j] = "g"
+#                 distance[j] = distance[u] + 1
+#                 p[j] = u
+#                 q.add(j)
+#         color[u] = "B"
+#     print(u)        
+
+# num_of_vertices = int(input())
+# q = queue()
+# color = ["W" for i in range(num_of_vertices)]
+# distance = [0 for i in range(num_of_vertices)]
+# p = [-1 for i in range(num_of_vertices)]
+# q.add(0)
+
+# while q.give_length() >= 0:
+#     item = q.pop()
+#     if item != -1:
+#         for i in range(num_of_vertices):
+#             if(adjacy[item][i] == 1):
+#                 if color[i] == "W":
+#                     color[i] = "G"
+#                     p[i] = item
+#                     distance[i] = distance[item] + 1
+#                     q.add(i)
+#         color[item] = "B"
+#         print(item)
+# print()
+# print(f"{color},{distance},{p}")
+
+
+# while q.give_length() - 1 >= 0:
+#     item = q.pop()
+#     if item != -1:
+#         for i in range(num_of_vertices ** 2):
+#             vertex = i % num_of_vertices
+#             drayer = int(input())
+#             if(drayer == 1):
+#                 if color[vertex] == "W":
+#                     color[vertex] = "G"
+#                     p[vertex] = item
+#                     distance[vertex] = distance[item] + 1
+#                     q.add(vertex)
+#             if(vertex == 0):
+#                 color[item] = "B"
+#             if(vertex == num_of_vertices - 1):
+#                 item = q.pop()
+#         print(item)
+
+# print(f"{color},{distance},{p}")
+# print()
+ 
+
+
+# def determin_path_to_destinationVertex(vertex):
+#     while(p[vertex] != -1):
+#         print(vertex)
+#         vertex = p[vertex]
+#     print(vertex)
+         
+# determin_path_to_destinationVertex(4) 
+
+
+    
+
+
+
+# def create_matrix(vertex_count):
+#     return [0 for i in range(vertex_count)]
+
+# vertex_count = int(input())
+# matrix = create_matrix(vertex_count)   
+        
+# for i in range(vertex_count**)
 
 
 
@@ -379,7 +513,7 @@ class queue:
         self.__list.append(num)
     
     def pop(self):
-        if(self.__length >= 0):
+        if(self.__length - 1 >= 0):
            item = self.__list[0]
            del self.__list[0]
            self.__length -= 1
@@ -405,20 +539,29 @@ class queue:
 
 class graph:
     
-    def __init__(self):
-        self.__vertices = int(input())
+    def __init__(self,vertices):
+        self.__vertices = vertices
         self.__dict = {}
+        self.__adjacy_matrices = []
         for i in range(self.__vertices):
             self.__dict[i] = []
 
-    
-    def create_graph_with_edge(self):
-        num_of_edges = int(input())
+    def create_graph_with_edge(self,num_of_edges):
+        num_of_edges = num_of_edges
         for i in range(num_of_edges):
             origin_vertice = int(input())
             destination_vertice = int(input())
             self.__dict[origin_vertice].append(destination_vertice)
             self.__dict[destination_vertice].append(origin_vertice)
+        return self.__dict
+
+    def create_adjacy_graph(self,drayer):
+        for i in range(self.__vertices):
+            self.__adjacy_matrices.append([])
+            for j in range(self.__vertices):
+                drayer = drayer
+                self.__adjacy_matrices[i].append(drayer) 
+        return self.__adjacy_matrices
         
     
     def get_num_of_vertices(self):
@@ -426,50 +569,106 @@ class graph:
     
     def get_graph(self):
         return self.__dict
-        
-        
-            
-
-
-grph = graph()
-create_grph = grph.create_graph_with_edge()
-num_of_vertices = grph.get_num_of_vertices()
-dic = grph.get_graph()
-print(dic)
-
-# bfs ======> 
-
-q = queue()
-color = ["w" for i in range(num_of_vertices)]
-distance = [0 for i in range(num_of_vertices)]
-p = [-1 for i in range(num_of_vertices)]
-
-q.add(0)
-
-while q.give_length() >= 0:
-    u = q.pop()
-    if u != -1:
-        for i in range(len(dic[u])):
-            j = dic[u][i]
-            if color[j] == "w":
-                color[j] = "g"
-                distance[j] = distance[u] + 1
-                p[j] = u
-                q.add(j)
-        color[u] = "B"
-    print(u)        
+    
+    def get_adjacy_matrices(self):
+        return self.__adjacy_matrices
         
 
 
+class BFS:
+
+   def __init__(self,dict_graph,num_of_vertices):
+       self.__dict_graph = dict_graph
+       self.__num_of_vertices = num_of_vertices
+       self.__queue = queue()
+       self.__distance_matrices = []
+       self.__color = ["w" for i in range(num_of_vertices)]
+       self.__distance = [0 for i in range(num_of_vertices)]
+       self.__parent = [-1 for i in range(num_of_vertices)]
+
+   def reset_C_D_P(self):
+        self.__color = ["w" for i in range(self.__num_of_vertices)]
+        self.__distance = [0 for i in range(self.__num_of_vertices)]
+        self.__parent = [-1 for i in range(self.__num_of_vertices)] 
+    
+   def get_distance_for_all_vertices(self):
+        for n in range(self.__num_of_vertices):
+            self.__queue.add(n)
+            while self.__queue.give_length() - 1 >= 0:
+                u = self.__queue.pop()
+                if u != -1:
+                    for i in range(len(self.__dict_graph[u])):
+                        j = self.__dict_graph[u][i]
+                        if self.__color[j] == "w":
+                            self.__color[j] = "g"
+                            self.__distance[j] = self.__distance[u] + 1
+                            self.__parent[j] = u
+                            self.__queue.add(j)
+                    self.__color[u] = "B"
+            self.__distance_matrices.append(self.__distance)
+            self.reset_C_D_P()    
+        return self.__distance_matrices
 
 
-        
-        
+   def get_distance_for_one_vertex_matrice_method(self):
+       while self.__queue.give_length() - 1 >= 0:
+        item = self.__queue.pop()
+        if item != -1:
+            for i in range(num_of_vertices ** 2):
+                vertex = i % self.__num_of_vertices
+                drayer = int(input())
+                if(drayer == 1):
+                    if self.__color[vertex] == "W":
+                        self.__color[vertex] = "G"
+                        self.__parent[vertex] = item
+                        self.__distance[vertex] = self.__distance[item] + 1
+                        q.add(vertex)
+                if(vertex == 0):
+                    self.__color[item] = "B"
+                if(vertex == num_of_vertices - 1):
+                    item = self.__queue.pop()
+            print(item)
+
+       print(f"{color},{distance},{p}")
+       self.reset_C_D_P()
+
+
+   def get_greatest_distance(self,distance_matrices):
+        greatest_num = 0
+        for i in range(self.__num_of_vertices):
+            for j in range(self.__num_of_vertices):
+                if(greatest_num < distance_matrices[i][j]):
+                    greatest_num = distance_matrices[i][j]
+        return greatest_num
+
+
+   def determin_path_to_destinationVertex(self,vertex):
+      self.__distance_matrices[vertex]
+      while(self.__parent[vertex] != -1):
+          print(vertex)
+          vertex = p[vertex]
+      print(vertex)
+         
+
     
     
+
+vertices = int(input())
+num_of_edges = int(input())
+def create_matrix(vertices,num_of_edges):
+    grph = graph(vertices)
+    return grph.create_graph_with_edge(num_of_edges)
     
-    
-    
-    
-    
-    
+
+def get_greatest_distance(dict_graph,vertices):
+    bfs = BFS(dict_graph, vertices)
+    distance_matrices = bfs.get_distance_for_all_vertices()
+    greatest_distance = bfs.get_greatest_distance(distance_matrices)
+    print()
+    print(f" greatest distance is : {greatest_distance}")
+
+
+
+
+dict_graph = create_matrix(vertices,num_of_edges)
+get_greatest_distance(dict_graph, vertices)
