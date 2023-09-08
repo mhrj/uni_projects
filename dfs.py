b0b9e1cdec0aaa43c3e14d529989d95d8380b94d
mhrj <mhrj007846@gmail.com>
2023-09-08 18:22:31 +0200

    cycle impelimented

--- dfs.py
+++ dfs.py
@@ -26,7 +26,6 @@ def find_components(num_of_nodes):
         if(visited[node.element] == False):
             counter += 1
             dfs(-1,node , counter)
-            print(f"{counter} : {components}")
             
 
 def dfs(p,node , counter):
@@ -43,19 +42,47 @@ def dfs(p,node , counter):
     adjacency_list.time += 1
     node.f = adjacency_list.time
 
-def find_cycles(starting_node):
-    u = starting_node
-    v = adjacency_list.list[starting_node.next.element]
-    while(v != None):
-        if(v.d > u.d and u.f > v.f and v.p != u.element):
-            u = v
-            v = adjacency_list.list[u.next.element]
-        else:
-            print("cycle") 
+    
+
+def create_path(index,p,s):
+    reconstructed_path = draft.stack()
+    reconstructed_path.add(s)
+    compare_node = s
+    for i in range(index,len(p)):
+        if (p[i][1] == compare_node[0]):
+            reconstructed_path.add(p[i])
+            compare_node = p[i]
+        if(s[1] == compare_node[0]): break
+    print(f"cycle on {s[1]} ==>")
+    print(f"path : {reconstructed_path.give_stack()}")
+        
+        
+def re_path(cycle_nodes,p):
+    for node in cycle_nodes:
+        for index,n in enumerate(p.give_stack()):
+            if(node == n):
+                create_path(index + 1,p.give_stack(),n)
+                break
+                
+def find_cycles():
+    p = draft.stack()
+    cycle_nodes = []
+    for target_node in adjacency_list.list:
+        next_node = target_node.next
+        while(next_node!= None):
+            v = adjacency_list.list[next_node.element]
+            if(target_node.d > v.d and target_node.f < v.f and target_node.p != v.element):
+                cycle_nodes.append([target_node.element,next_node.element])
+            p.add([target_node.element,next_node.element])
+            next_node = next_node.next    
+
+    if(cycle_nodes != []):
+        re_path(cycle_nodes,p)
+    else:
+        print("there is no cycle!")
 
 
 find_components(number_of_nodes)
-print(adjacency_list)
-find_cycles(adjacency_list.list[0])
+find_cycles()
 
 
